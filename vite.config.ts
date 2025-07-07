86721d11ea20a5a92b9d89f282c6c3d80fe8eca9
Kauan Cerniauskas <kauankg@hotmail.com>
2025-07-07 13:59:41 -0300

    atualizado vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import { VitePWA } from 'vite-plugin-pwa';
+import manifest from './public/manifest.json' assert { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -16,7 +17,7 @@ export default defineConfig(({ mode }) => ({
     componentTagger(),
     VitePWA({
       registerType: 'autoUpdate',
-      manifest: require('./public/manifest.json'),
+      manifest,
       includeAssets: ['favicon.ico', 'logo-192.png', 'logo-512.png', 'logo-maskable.png'],
       devOptions: {
         enabled: true
